642926a2033fef075b12052986395ce07a06f8a0
root <root@TriThuc>
2024-04-19 20:33:47 +0700

    init commit for deployment

---
+++ src/api/config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+  // baseURL: "http://localhost:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
+  baseURL: "http://18.143.74.162/api", // Thay đổi địa chỉ IP của máy chủ tại đây
+};
+
+export default config;
